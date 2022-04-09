dcd2c852f21e5d9ec66a042902b8c790dd354099
Gabriel Proença Alves <gproenca42@gmail.com>
2022-04-09 00:15:12 -0300

    Update pomodoro.py

--- pomodoro.py
+++ pomodoro.py
@@ -31,102 +31,105 @@ while True:
 
 #--------------------------------------------------------------
     try:
-        UserInput = leiaint('Insert: ')
+        UserInput = leiaint__('Insert: ')
 
 
     except(KeyboardInterrupt):
-        confirm(ui=['Are you sure? [y/n]', 'Insert: '], yes='y', no='n')
+        clear_scrn()
+        print('INVALID OPTION!')
+    else:
 #--------------------------------------------------------------
 
 
 #--------------------------------------------------------------
-    try:
+        try:
 
 
-        if UserInput == 1: # 25 minutes
-            for timer in range(0, 1, 1):
-                sleep(1500)
-                notify('TIME OUT!', '25 MINUTES HAVE PASSED!\n', 10000)
-                alarm(audio=Audio_Set)
-                clear_scrn()
-        
-
-        elif UserInput == 2: # 5 minutes
-            for timer in range(0, 1, 1):
-                sleep(300)
-                notify('TIME OUT!', '5 MINUTES HAVE PASSED!\n', 10000)
-                alarm(audio=Audio_Set)
-                clear_scrn()
-
-
-        elif UserInput == 3: # BOTH
-            for timer in range(0, 1, 1):
-                sleep(1500)
-                notify('TIME OUT!', '25 MINUTES HAVE PASSED!\n', 10000)
-                alarm(audio=Audio_Set)
-                sleep(300)
-                notify('TIME OUT!', '5 MINUTES HAVE PASSED!\n', 10000)
-                alarm(audio=Audio_Set)
-            clear_scrn()
-
-        
-        elif UserInput == 4: # BOTH  + REPLAY
-            while True:
-                sleep(1500)
-                notify('TIME OUT!', '25 MINUTES HAVE PASSED!\n', 10000)
-                alarm(audio=Audio_Set)
-                sleep(300)
-                notify('TIME OUT!', '5 MINUTES HAVE PASSED!\n', 10000)
-                alarm(audio=Audio_Set)
-            clear_scrn()
-
-    except(KeyboardInterrupt):
-        print('\nOperation Interrupted!\n')
-#--------------------------------------------------------------
-
-
-#--------------------------------------------------------------
-    if UserInput == 5: # Set Alarm
-        
-
-            while True:
-                title(texto='AUDIO LIST', tamfai=32, faixa='-')
-                menu(lista=audio_list)
+            if UserInput == 1: # 25 minutes
+                for timer in range(0, 1, 1):
+                    sleep(1500)
+                    notify('TIME OUT!', '25 MINUTES HAVE PASSED!\n', 10000)
+                    alarm(audio=Audio_Set)
+                    clear_scrn()
             
 
-                try:
-                    User_Input = leiaint__('Insert [1 - 6]: ')
-                except KeyboardInterrupt:
+            elif UserInput == 2: # 5 minutes
+                for timer in range(0, 1, 1):
+                    sleep(300)
+                    notify('TIME OUT!', '5 MINUTES HAVE PASSED!\n', 10000)
+                    alarm(audio=Audio_Set)
                     clear_scrn()
-                    break
-                except:
-                    clear_scrn()
-                    break
-                else:
-
-                    if User_Input < 1 or User_Input > 6:
-                        print('INVALID OPTION!')
-                    
-
-                    else:
-                        arch_clear('audio.conf')
-                        arch_write('audio.conf', 'blibs/sfx/' + audio_list[User_Input - 1] + '.mp3')
-                        clear_scrn()
-                        break						
-#--------------------------------------------------------------
 
 
-#--------------------------------------------------------------
-    elif UserInput == 6: # Exit
+            elif UserInput == 3: # BOTH
+                for timer in range(0, 1, 1):
+                    sleep(1500)
+                    notify('TIME OUT!', '25 MINUTES HAVE PASSED!\n', 10000)
+                    alarm(audio=Audio_Set)
+                    sleep(300)
+                    notify('TIME OUT!', '5 MINUTES HAVE PASSED!\n', 10000)
+                    alarm(audio=Audio_Set)
+                clear_scrn()
 
-        if confirm(ui=['Are you sure? [y/n]', 'Insert: '], yes='y', no='n'):
+            
+            elif UserInput == 4: # BOTH  + REPLAY
+                while True:
+                    sleep(1500)
+                    notify('TIME OUT!', '25 MINUTES HAVE PASSED!\n', 10000)
+                    alarm(audio=Audio_Set)
+                    sleep(300)
+                    notify('TIME OUT!', '5 MINUTES HAVE PASSED!\n', 10000)
+                    alarm(audio=Audio_Set)
+                clear_scrn()
+
+        except(KeyboardInterrupt):
             clear_scrn()
-            exit()
+            print('\nOperation Interrupted!\n')
+#--------------------------------------------------------------
+
+
+#--------------------------------------------------------------
+        if UserInput == 5: # Set Alarm
+            
+
+                while True:
+                    title(texto='AUDIO LIST', tamfai=32, faixa='-')
+                    menu(lista=audio_list)
+                
+
+                    try:
+                        User_Input = leiaint__('Insert [1 - 6]: ')
+                    except KeyboardInterrupt:
+                        clear_scrn()
+                        break
+                    except:
+                        clear_scrn()
+                        break
+                    else:
+
+                        if User_Input < 1 or User_Input > 6:
+                            print('INVALID OPTION!')
+                        
+
+                        else:
+                            arch_clear('audio.conf')
+                            arch_write('audio.conf', 'blibs/sfx/' + audio_list[User_Input - 1] + '.mp3')
+                            clear_scrn()
+                            break						
+#--------------------------------------------------------------
+
+
+#--------------------------------------------------------------
+        elif UserInput == 6: # Exit
+
+            if confirm(ui=['Are you sure? [y/n]', 'Insert: '], yes='y', no='n'):
+                clear_scrn()
+                exit()
+            
+            else:
+                continue
         
-        else:
-            continue
-    
-    elif UserInput > 6 or UserInput < 1:
-        print('INVALID OPTION!')
+        elif UserInput > 6 or UserInput < 1:
+            print('INVALID OPTION!')
 #--------------------------------------------------------------
 
