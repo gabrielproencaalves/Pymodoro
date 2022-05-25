d831503c8d3fb30c591018d63dc2b5caef2afbb4
Gabriel Proença Alves <gproenca42@gmail.com>
2022-05-24 21:19:11 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 sudo cp pomodoro /bin
 mkdir ~/.pymodoro
-cp -r ../Pymodoro ~/.pymodoro
+cp -r $PWD ~/.pymodoro
+clear
 echo "You can now exec the app just typping (pymodoro) in your terminal"
+echo "you can now delete this folder, the new program directory is located in ~/.pymodoro"
